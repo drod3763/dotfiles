ebfd375b8e6b527d8f6ce6141ce94f0ab2e6f709
Derick Rodriguez <7785716+drod3763@users.noreply.github.com>
2023-10-04 16:20:50 -0400

    Updates

--- home/.chezmoiscripts/macOS/run_after_install_printers.sh
+++ home/.chezmoiscripts/macOS/run_after_install_printers.sh
@@ -5,19 +5,19 @@ set -euo pipefail
 # Don't error when no files are found
 shopt -s nullglob
 
-FILES="${HOME}/Downloads/Helpers/*.dmg"
+FILES="${HOME}/Downloads/Printers/*.dmg"
 
 for FILE in ${FILES}; do
-    # Mount the file as a volume
-    APPS_FOLDER='/Applications';
+    # Mount the file as a volume. Note that the printer drivers are nested underneath a folder structure starting from Root.
+    PRINTER_FOLDER='/';
     echo "Mounting ${FILE} image";
     VOLUME=$(hdiutil mount "${FILE}" | tail -n1 | perl -nle '/(\/Volumes\/[^\b]+)/; print $1');
 
     # Locate .app folder and move to /Applications
     echo "Attempting to locate ${VOLUME}";
     APP=$(find "${VOLUME}"/ -name '*.app' -maxdepth 1 -type d -print0);
-    echo "Copying $(echo "${APP}" | awk -F/ '{print $NF}') into ${APPS_FOLDER}...";
-    \cp -iR "${APP}" "${APPS_FOLDER}";
+    echo "Copying $(echo "${APP}" | awk -F/ '{print $NF}') into ${PRINTER_FOLDER}...";
+    \cp -iR "${APP}" "${PRINTER_FOLDER}";
 
     # Unmount volume, delete temporary file
     echo "Cleaning up...";
