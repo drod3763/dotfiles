72cd9ea697118d6846cb773fd4a2d47ad527da56
Derick Rodriguez <7785716+drod3763@users.noreply.github.com>
2023-10-12 19:09:55 -0400

    Updates

--- home/.chezmoiscripts/macOS/run_after_install_printers.sh
+++ home/.chezmoiscripts/macOS/run_after_install_printers.sh
@@ -9,16 +9,15 @@ FILES="${HOME}/Downloads/Printers/*.dmg"
 
 for FILE in ${FILES}; do
     # Mount the file as a volume. Note that the printer drivers are nested underneath a folder structure starting from Root.
-    PRINTER_FOLDER='/';
+    ROOT="/";
     echo "Mounting ${FILE} image";
     VOLUME=$(hdiutil mount "${FILE}" | tail -n1 | perl -nle '/(\/Volumes\/[^\b]+)/; print $1');
 
     # Locate .app folder and move to /Applications
-    echo "Attempting to locate ${VOLUME}";
-    APP=$(find "${VOLUME}"/ -name '*.app' -maxdepth 1 -type d -print0);
-    echo "Copying $(echo "${APP}" | awk -F/ '{print $NF}') into ${PRINTER_FOLDER}...";
-    \cp -iR "${APP}" "${PRINTER_FOLDER}";
-
+    echo "Attempting to locate the volume ${VOLUME}...";
+    PKG=$(find "${VOLUME}" -name '*.pkg' -maxdepth 1 -type f -print0);
+    echo "Installing $(echo "${PKG}" | awk -F/ '{print $NF}') into ${ROOT}...";
+    sudo installer -pkg "${PKG}" -target "${ROOT}"
     # Unmount volume, delete temporary file
     echo "Cleaning up...";
     hdiutil unmount "${VOLUME}" -quiet;
